e2cf1623fb868e3aa849becbd3324c0336748c90
Jani Keränen <jani_keranen@hotmail.com>
2019-12-02 22:15:40 +0200

    'adde'

---
+++ bot.py
@@ -0,0 +1,53 @@
+import requests
+import json
+import datetime
+from datetime import timezone
+
+
+def load_json_data():
+    url = "https://adventofcode.com/2019/leaderboard/private/view/21796.json"
+    session_id = "53616c7465645f5ff1385fd71f5e21ee34318254e8084f9322104e5fc2de36ec6e9e75141fc61f7bc177c2872a7acdd1"
+    cookie = {"session": session_id}
+
+    post_response = requests.post(url, cookies=cookie)
+    json_data = json.loads(post_response.text)
+
+    return json_data
+
+
+def send_slack_msg(name, challenge, part, stars, points):
+    webhook_url = ""
+    msg = "{} solved puzzle {} part {} and has now {} :star: and {} points. Congratulations!".format(name, challenge, part, stars, points)
+    payload = '{"text" : "' + msg + '"}'
+    req = requests.Request(webhook_url, headers={"Content-Type":"application/x-www-form-urlencoded"}, data=payload.encode('utf-8'))
+
+
+
+def find_newest_completion():
+    json = load_json_data()
+
+    for id in json["members"]:
+        member_object = json["members"][id]
+        name_st = member_object["name"]
+        stars_st = member_object["stars"]
+        points_st = member_object["local_score"]
+
+        dt = datetime.datetime.now()
+        last_star_ts = member_object["last_star_ts"]
+
+        if dt.replace(tzinfo=timezone.utc).timestamp() - int(last_star_ts) < 61:
+
+            for challenges in member_object["completion_day_level"]:
+                challenge_days = member_object["completion_day_level"][challenges]
+
+                for day in challenge_days:
+                    part_times = challenge_days[day]
+                    for star_time in part_times:
+                        if int(part_times[star_time]) == int(last_star_ts):
+                            day_ts = challenges
+                            part_ts = day
+
+                            send_slack_msg(name_st, day_ts, part_ts, stars_st, points_st)
+
+
+find_newest_completion()
